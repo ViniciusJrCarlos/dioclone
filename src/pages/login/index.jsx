389f07f5c73eed761b7e6afc4f7a23cc807da61a
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-07-03 22:46:28 -0300

    iniciando projeto do dioclone

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+const Login = () => {
+
+    return (<>
+    
+
+      <h1>Home</h1>
+      <Link to="/">Voltar pra Home</Link>
+     
+     </>)
+
+}
+
+export {Login};
